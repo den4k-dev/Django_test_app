32f01d1ff6d5d6c156f9707aa998b1b7c2403953
den4kaden4k <den.kor137@yandex.ru>
2021-11-29 18:27:52 +0300

    added templates base, index

--- stuff_list/index_app/views.py
+++ stuff_list/index_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    content = {
+        'title': 'Stuff list',
+    }
+    return render(request, 'index.html', context=content)
